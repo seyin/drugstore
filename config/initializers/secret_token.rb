a1cbf2f1b6252ce10abf4dbcb84e18f8baabf12a
Selin Gomez <seyin@SelinSatellite.(none)>
2012-05-28 17:52:20 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drugstore::Application.config.secret_token = '483c1ce498b022beb08fd1e45c3be9ab35c55dd1af4a39d90c63d40c9c72c6ffbd6eeb9a1babbb08c10e2b579b8279c1991c04a8890a75840e890b4272dd8bbb'
