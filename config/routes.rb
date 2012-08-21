482c787da3765c724b22f277c257bd37a0becea2
Selin Gomez Abularach <sgabula@gmail.com>
2012-08-21 11:24:50 -0400

    - Ahora ya podemos loguearnos una sola vez y tambien acceder a la seccion admin sin loguearnos de nuevo.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Drugstore::Application.routes.draw do
 
   ActiveAdmin.routes(self)
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
+  #devise_for :admin_users, ActiveAdmin::Devise.config
 
   devise_for :users
 
